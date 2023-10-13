44cce1f478322b3e52755c7b1f4ce14a3180e648
smwemeke <sue.mwemeke@gmail.com>
2023-10-13 14:49:50 -0500

    Changed name of system

--- src/librarysystem/Main.java
+++ src/librarysystem/Main.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 
-
 public class Main {
 
 	public static void main(String[] args) {
 	      EventQueue.invokeLater(() -> 
 	         {
-	            LibrarySystem.INSTANCE.setTitle("Fairfield Public Library Application");
+	            LibrarySystem.INSTANCE.setTitle("Elite Library System");
 	            LibrarySystem.INSTANCE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	            
 	            LibrarySystem.INSTANCE.init();
